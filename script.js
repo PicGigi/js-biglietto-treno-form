32ee61a4baf1d3e4a6a009e3a4f9a3ccebb6c54c
PicGigi <junior.1997@live.it>
2022-10-21 17:20:17 +0200

    prova

--- script.js
+++ script.js
@@ -2,34 +2,32 @@ const element = document.getElementById("cliccami");
 element.addEventListener('click', 
     function() {
         // NOME
-        const nome = document.querySelector('[name=nome]').value;
-        document.getElementById('nome').innerHTML = nome;
+        const nome = document.getElementById("nome").value;
+        document.getElementById('nomeBiglietto').innerHTML = nome;
         console.log( "Il nome è " + nome);
         
         // ETA'
-        const eta = document.querySelector('[name=eta]').value;
+        const eta = document.getElementById("eta").value;
         console.log("L'età è: " + eta)
 
         // PREZZO
-        if (età = 1)
+        if (eta == 1)
         {
-            const prezzo = parseFloat((document.querySelector('[name=distanza]').value * 0.21) * 0.80).toFixed(2);
+            const prezzo = parseFloat((document.getElementById("distanza").value * 0.21) * 0.80).toFixed(2);
             console.log("Sconto minorenne del 20%")
             console.log("Il prezzo è " + prezzo);
             document.getElementById('prezzo').innerHTML = prezzo;
         }
-
-        else if (età = 2) 
+        else if (eta == 2) 
         {
-            const prezzo = parseFloat(document.querySelector('[name=distanza]').value * 0.21).toFixed(2);
+            const prezzo = parseFloat(document.getElementById("distanza").value * 0.21).toFixed(2);
             console.log("Nessuno sconto")
             console.log("Il prezzo è " + prezzo);
             document.getElementById('prezzo').innerHTML = prezzo;
         }
-
-        else if (età = 3)
+        else if (eta == 3)
         {
-            const prezzo = parseFloat((document.querySelector('[name=distanza]').value * 0.21) * 0.60).toFixed(2);
+            const prezzo = parseFloat((document.getElementById("distanza").value * 0.21) * 0.60).toFixed(2);
             console.log("Sconto over 65 del 40%")
             console.log("Il prezzo è " + prezzo);
             document.getElementById('prezzo').innerHTML = prezzo;
